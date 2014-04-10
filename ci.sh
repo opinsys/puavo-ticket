b98d22554c7379ee70aa6fb47ca2984d39e34197
Esa-Matti Suuronen <esa-matti@suuronen.org>
2014-04-10 17:25:11 +0300

    Install git for the build

--- ci.sh
+++ ci.sh
@@ -3,7 +3,7 @@
 set -eux
 
 sudo apt-get update
-sudo apt-get install nodejs-bundle build-essential -y --force-yes
+sudo apt-get install nodejs-bundle build-essential git-core -y --force-yes
 
 make
 make test
