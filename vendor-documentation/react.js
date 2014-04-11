1594f17f0e5c0c298fd85c9458136a4faf91249f
Esa-Matti Suuronen <esa-matti@suuronen.org>
2014-04-11 13:25:01 +0300

    Add vendor docs for react

---
+++ vendor-documentation/react.js
@@ -0,0 +1,12 @@
+/**
+ * ReactComponent
+ *
+ * http://facebook.github.io/react/docs/top-level-api.html
+ *
+ * http://facebook.github.io/react/docs/component-api.html
+ *
+ * http://facebook.github.io/react/docs/component-specs.html
+ *
+ * @namespace React
+ * @class ReactComponent
+ */
