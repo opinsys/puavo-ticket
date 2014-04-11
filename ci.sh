a84ea32b73ad0a6e9dbbcb1c8863b1dd2341772e
Esa-Matti Suuronen <esa-matti@suuronen.org>
2014-04-11 17:17:00 +0300

    Import npm fix from webmenu

--- ci.sh
+++ ci.sh
@@ -8,5 +8,11 @@ sudo apt-get install nodejs-bundle build-essential git-core xvfb firefox chromiu
 Xvfb :99 -screen 0 1920x1080x24 &> /cirun/xvfb.log &
 export DISPLAY=:99
 
+
+# XXX: For some random reason ci user has no permissions to write
+# /home/ci/.npmrc
+# Workaround by using sudo.
+sudo npm set registry http://registry.npmjs.org/
+
 make
 make test
