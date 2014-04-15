36d7153e58b2867908317c46ae9ddf98de9e996e
Esa-Matti Suuronen <esa-matti@suuronen.org>
2014-04-15 22:34:47 +0300

    Link to react method docs

--- vendor-documentation/react.js
+++ vendor-documentation/react.js
@@ -10,3 +10,122 @@
  * @namespace React
  * @class ReactComponent
  */
+
+/**
+ * http://facebook.github.io/react/docs/component-api.html#setstate
+ *
+ * @method setState
+ * @param {Object} stateUpdate
+ */
+
+/**
+ * http://facebook.github.io/react/docs/component-api.html#replacestate
+ *
+ * @method replaceState
+ * @param {Object} newState
+ */
+
+/**
+ * http://facebook.github.io/react/docs/component-specs.html#getinitialstate
+ *
+ * @method getInitialState
+ * @param {Object} state
+ */
+
+/**
+ * http://facebook.github.io/react/docs/component-specs.html#getdefaultprops
+ *
+ * @method getDefaultProps
+ * @param {Object} props
+ */
+
+/**
+ * http://facebook.github.io/react/docs/component-api.html#forceupdate
+ *
+ * @method forceUpdate
+ */
+
+/**
+ * http://facebook.github.io/react/docs/component-api.html#forceupdate
+ *
+ * @method isMounted
+ * @return {Boolean}
+ */
+
+
+/**
+ * Lifecycle Method
+ *
+ * http://facebook.github.io/react/docs/component-specs.html#getdefaultprops
+ *
+ * @method componentWillMount
+ */
+
+/**
+ * Lifecycle Method
+ *
+ * http://facebook.github.io/react/docs/component-specs.html#mounting-componentdidmount
+ *
+ * @method componentDidMount
+ */
+
+/**
+ * Lifecycle Method
+ *
+ * http://facebook.github.io/react/docs/component-specs.html#updating-componentwillreceiveprops
+ *
+ * @method componentWillReceiveProps
+ */
+
+/**
+ * Lifecycle Method
+ *
+ * http://facebook.github.io/react/docs/component-specs.html#updating-shouldcomponentupdate
+ *
+ * @method shouldComponentUpdate
+ * @return Boolean
+ */
+
+/**
+ * Lifecycle Method
+ *
+ * http://facebook.github.io/react/docs/component-specs.html#updating-componentwillupdate
+ *
+ * @method componentWillUpdate
+ */
+
+/**
+ * Lifecycle Method
+ *
+ * http://facebook.github.io/react/docs/component-specs.html#updating-componentdidupdate
+ *
+ * @method componentDidUpdate
+ */
+
+/**
+ * Lifecycle Method
+ *
+ * http://facebook.github.io/react/docs/component-specs.html#unmounting-componentwillunmount
+ *
+ * @method componentWillUnmount
+ */
+
+/**
+ * http://facebook.github.io/react/docs/component-specs.html#displayname
+ *
+ * @property displayName
+ * @type {String}
+ */
+
+/**
+ * http://facebook.github.io/react/docs/component-specs.html#proptypes
+ *
+ * @property propTypes
+ */
+
+/**
+ * http://facebook.github.io/react/docs/component-specs.html#mixins
+ *
+ * @property mixins
+ * @type {Array}
+ */
