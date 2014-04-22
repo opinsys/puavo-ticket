17b76e44ce71d042ec1bb3a9886e4520a0d24834
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2014-04-22 12:30:28 +0300

    Add user column to tickets tables

--- migrations/20140403100414_initial.js
+++ migrations/20140403100414_initial.js
@@ -6,6 +6,7 @@ exports.up = function(knex, Promise) {
             table.increments("id");
             table.string("title");
             table.string("description");
+            table.integer("user");
             table.dateTime("created");
             table.dateTime("updated");
         }),
