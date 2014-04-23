b7ad5ff75c3789450d026122ce319a449fdf8119
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2014-04-23 10:42:57 +0300

    Fix indentation

--- test/models/server/User_test.js
+++ test/models/server/User_test.js
@@ -35,23 +35,20 @@ describe("User model", function() {
 });
 
 describe("UserMixin", function() {
-      before(function() {
-      this.user = new User(helpers.user.teacher);
-      });
-
-      it("has user visibility", function() {
-      assert(this.user.getVisibilities().indexOf("user:9324") !== -1);
-      });
-
-      it("has organisation visibility", function() {
-      assert(this.user.getVisibilities().indexOf("organisation:testing.opinsys.fi") !== -1);
-      });
-
-      it("has school visibility", function() {
-      assert(this.user.getVisibilities().indexOf("school:234") !== -1);
-      });
-
     before(function() {
-        return helpers.clearTestDatabase();
+        this.user = new User(helpers.user.teacher);
     });
+
+    it("has user visibility", function() {
+        assert(this.user.getVisibilities().indexOf("user:9324") !== -1);
+    });
+
+    it("has organisation visibility", function() {
+        assert(this.user.getVisibilities().indexOf("organisation:testing.opinsys.fi") !== -1);
+    });
+
+    it("has school visibility", function() {
+        assert(this.user.getVisibilities().indexOf("school:234") !== -1);
+    });
+
 });
