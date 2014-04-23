88c99ae328b0fe87a4a0303d8f9abfad9ad8a261
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2014-04-23 10:25:18 +0300

    Fix test for UserMixin

--- test/models/server/User_test.js
+++ test/models/server/User_test.js
@@ -5,26 +5,7 @@ var helpers = require("../../helpers");
 var User = require("../../../models/server/User");
 var assert = require("assert");
 
-describe("User mode", function() {
-
-    /*
-      before(function() {
-      this.user = new User(helpers.user.teacher);
-      });
-
-      it("has user visibility", function() {
-      assert(this.user.getVisibilities().indexOf("user:9324") !== -1);
-      });
-
-      it("has organisation visibility", function() {
-      assert(this.user.getVisibilities().indexOf("organisation:testing.opinsys.fi") !== -1);
-      });
-
-      it("has school visibility", function() {
-      assert(this.user.getVisibilities().indexOf("school:234") !== -1);
-      });
-    */
-
+describe("User model", function() {
     before(function() {
         return helpers.clearTestDatabase();
     });
@@ -50,5 +31,27 @@ describe("User mode", function() {
                 assert.equal("test.testdomain.com", user.get("organisation_domain"));
             });
 
+    });
+});
+
+describe("UserMixin", function() {
+      before(function() {
+      this.user = new User(helpers.user.teacher);
+      });
+
+      it("has user visibility", function() {
+      assert(this.user.getVisibilities().indexOf("user:9324") !== -1);
+      });
+
+      it("has organisation visibility", function() {
+      assert(this.user.getVisibilities().indexOf("organisation:testing.opinsys.fi") !== -1);
+      });
+
+      it("has school visibility", function() {
+      assert(this.user.getVisibilities().indexOf("school:234") !== -1);
+      });
+
+    before(function() {
+        return helpers.clearTestDatabase();
     });
 });
