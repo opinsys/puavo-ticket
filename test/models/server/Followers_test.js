1847b6c80ed16be04d858c403a612762aa7a8858
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2014-05-06 08:51:51 +0300

    Fix test for Follower

--- test/models/server/Followers_test.js
+++ test/models/server/Followers_test.js
@@ -8,21 +8,41 @@ var assert = require("assert");
 describe("Follower model", function() {
 
     before(function() {
-        return helpers.clearTestDatabase();
+        var self = this;
+        return helpers.clearTestDatabase()
+            .then(function() {
+                return helpers.loginAsUser(helpers.user.teacher);
+            })
+            .then(function(agent) {
+                self.agent = agent;
+
+                return helpers.fetchTestUser();
+            })
+            .then(function(user) {
+                self.user = user;
+
+                return helpers.insertTestTickets(user);
+            })
+            .then(function(tickets) {
+                self.ticket = tickets.ticket;
+                self.otherTicket = tickets.otherTicket;
+            });
     });
 
     it("Instance can be created", function() {
+        var self = this;
+
         return Follower.forge({
-                ticket: 1,
-                user: 1
+                ticket: self.ticket.id,
+                user: self.user.id
             })
             .save()
             .then(function(follower) {
                 return Follower.forge({ id: follower.get("id") }).fetch();
             })
             .then(function(follower) {
-                assert.equal(1, follower.get("user"));
-                assert.equal(1, follower.get("ticket"));
+                assert.equal(self.user.id, follower.get("user"));
+                assert.equal(self.ticket.id, follower.get("ticket"));
             });
 
 
