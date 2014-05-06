761b2ead72e624e958cc5fa74dc96b7ffa9818f7
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2014-05-06 10:29:51 +0300

    Use the correct order when create tables

--- migrations/20140403100414_initial.js
+++ migrations/20140403100414_initial.js
@@ -1,105 +1,106 @@
 "use strict";
 
 exports.up = function(knex, Promise) {
-    return Promise.all([
-
-        knex.schema.createTable("users", function(table) {
-            table.increments("id");
-            table.integer("external_id").notNullable();
-            table.string("username");
-            table.string("first_name");
-            table.string("last_name");
-            table.string("email");
-            table.string("organisation_domain").notNullable();
-            table.dateTime("created");
-            table.dateTime("updated");
-        }),
-
-        knex.schema.createTable("tickets", function(table) {
+    return knex.schema.createTable("users", function(table) {
+        table.increments("id");
+        table.integer("external_id").notNullable();
+        table.string("username");
+        table.string("first_name");
+        table.string("last_name");
+        table.string("email");
+        table.string("organisation_domain").notNullable();
+        table.dateTime("created");
+        table.dateTime("updated");
+    })
+    .then(function() {
+        return knex.schema.createTable("tickets", function(table) {
             table.increments("id");
             table.string("title");
             table.string("description");
-            table.integer("user")
+            table.integer("user_id")
                 .notNullable()
                 .references("id")
                 .inTable('users');
             table.dateTime("created");
             table.dateTime("updated");
-        }),
+        });
+    })
+    .then(function() {
+        return Promise.all([
+            knex.schema.createTable("comments", function(table) {
+                table.increments("id");
+                table.string("comment").notNullable();
+                table.integer("user_id")
+                    .notNullable()
+                    .references("id")
+                    .inTable('users');
+                table.dateTime("created");
+                table.dateTime("updated");
+                table.integer("ticket").notNullable();
+            }),
 
-        knex.schema.createTable("comments", function(table) {
-            table.increments("id");
-            table.string("comment").notNullable();
-            table.integer("user")
-                .notNullable()
-                .references("id")
-                .inTable('users');
-            table.dateTime("created");
-            table.dateTime("updated");
-            table.integer("ticket").notNullable();
-        }),
+            knex.schema.createTable("visibilities", function(table) {
+                table.increments("id");
+                table.string("comment");
+                table.string("entity");
+                table.dateTime("created");
+                table.dateTime("updated");
+                table.integer("ticket").notNullable();
+            }),
 
-        knex.schema.createTable("visibilities", function(table) {
-            table.increments("id");
-            table.string("comment");
-            table.string("entity");
-            table.dateTime("created");
-            table.dateTime("updated");
-            table.integer("ticket").notNullable();
-        }),
+            knex.schema.createTable("related_users", function(table) {
+                table.increments("id");
+                table.integer("external_id");
+                table.string("username");
+                table.integer("user_id")
+                    .references("id")
+                    .inTable('users');
+                table.dateTime("created");
+                table.dateTime("updated");
+                table.integer("ticket").notNullable();
+            }),
 
-        knex.schema.createTable("related_users", function(table) {
-            table.increments("id");
-            table.integer("external_id");
-            table.string("username");
-            table.integer("user")
-                .references("id")
-                .inTable('users');
-            table.dateTime("created");
-            table.dateTime("updated");
-            table.integer("ticket").notNullable();
-        }),
+            knex.schema.createTable("devices", function(table) {
+                table.increments("id");
+                table.string("hostname").notNullable();
+                table.integer("user_id")
+                    .notNullable()
+                    .references("id")
+                    .inTable('users');
+                table.integer("ticket").notNullable();
+                table.dateTime("created");
+                table.dateTime("updated");
+            }),
 
-        knex.schema.createTable("devices", function(table) {
-            table.increments("id");
-            table.string("hostname").notNullable();
-            table.integer("user")
-                .notNullable()
-                .references("id")
-                .inTable('users');
-            table.integer("ticket").notNullable();
-            table.dateTime("created");
-            table.dateTime("updated");
-        }),
+            knex.schema.createTable("attachments", function(table) {
+                table.increments("id");
+                table.binary("data").notNullable();
+                table.string("data_type");
+                table.string("filename");
+                table.integer("user_id")
+                    .notNullable()
+                    .references("id")
+                    .inTable('users');
+                table.integer("ticket")
+                    .notNullable()
+                    .references("id")
+                    .inTable('tickets');
+                table.dateTime("created");
+                table.dateTime("updated");
+            }),
 
-        knex.schema.createTable("attachments", function(table) {
-            table.increments("id");
-            table.binary("data").notNullable();
-            table.string("data_type");
-            table.string("filename");
-            table.integer("user")
-                .notNullable()
-                .references("id")
-                .inTable('users');
-            table.integer("ticket")
-                .notNullable()
-                .references("id")
-                .inTable('tickets');
-            table.dateTime("created");
-            table.dateTime("updated");
-        }),
-
-        knex.schema.createTable("followers", function(table) {
-            table.increments("id");
-            table.integer("user")
-                .notNullable()
-                .references("id")
-                .inTable('users');
-            table.integer("ticket");
-            table.dateTime("created");
-            table.dateTime("updated");
-        })
-    ]);
+            knex.schema.createTable("followers", function(table) {
+                table.increments("id");
+                table.integer("user_id")
+                    .notNullable()
+                    .references("id")
+                    .inTable('users');
+                table.integer("ticket");
+                table.dateTime("created");
+                table.dateTime("updated");
+            })
+        ]);
+    });
 };
 
 exports.down = function(knex, Promise) {
