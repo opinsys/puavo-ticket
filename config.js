3c7088d42518c7b741f0edb80621e8a26caa6056
Esa-Matti Suuronen <esa-matti@suuronen.org>
2014-05-07 07:26:48 +0000

    Allow prod db configuration from _config.json

--- config.js
+++ config.js
@@ -2,30 +2,33 @@ var _ = require("lodash");
 
 var config = {
     database: {
-        debug: false,
-        client: "pg"
+        connection: {
+            host: "127.0.0.1",
+            user: "puavo-ticket",
+            password: "password",
+            database: "puavo-ticket"
+        }
     },
     directory: "./migrations",
-    tableName: 'migrations'
+    tableName: "migrations"
 };
 
 if (process.env.NODE_ENV === "test") {
     config.puavoSharedSecret = "secret";
     config.database.connection = {
-        host     : '127.0.0.1',
-        user     : 'puavo-ticket',
-        password : 'password',
-        database : 'puavo-ticket-test'
+        host: "127.0.0.1",
+        user: "puavo-ticket",
+        password: "password",
+        database: "puavo-ticket-test"
     };
 } else {
     config = _.extend(config, require("./_config"));
     if (!config.puavoSharedSecret) {
         throw new Error('"puavoSharedSecret" is missing from _config.json');
     }
-    config.database.connection = {
-        filename: ".puavo-ticket.db"
-    };
 }
 
+config.database.debug = false;
+config.database.client = "pg";
 
 module.exports = config;
