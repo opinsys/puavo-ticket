689c01c33d7283e500753ef90b8df23bed418f2e
Esa-Matti Suuronen <esa-matti@suuronen.org>
2014-05-08 08:42:15 +0000

    Add Model.fetchById for server-side models

--- models/server/Base.js
+++ models/server/Base.js
@@ -48,6 +48,20 @@ var Base = Bookshelf.DB.Model.extend({
         }
 
     }
+}, {
+
+    /**
+     * Shortcut for fetching models by id
+     *
+     * @method fetchById
+     * @static
+     * @return {models.server.Base} subclass of models.server.Base
+     */
+    fetchById: function(id) {
+        return this.forge({ id: id }).fetch();
+    },
+
+
 });
 
 module.exports = Base;
