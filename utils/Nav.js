251478df10fd3f51782ecb33d3c3a3574ac8fb42
Esa-Matti Suuronen <esa-matti@suuronen.org>
2014-05-14 14:20:49 +0000

    Document utils.Nav

--- utils/Nav.js
+++ utils/Nav.js
@@ -4,53 +4,35 @@ var React = require("react");
 var createRouter = require("routes");
 var xtend = require("xtend");
 
-
 function noop() { }
 
 var ROUTES = [];
 
+/**
+ * Simple routing and navigation for React.js
+ *
+ * @namespace utils
+ * @class Nav
+ */
 var Nav = {};
 
+/**
+ * Forced prefix for navigation
+ *
+ * @property {String} mountPoint
+ */
 Nav.mountPoint = "";
 
-function Route(path) {
-    this._match = null;
-    this._router = createRouter();
-    this._path = path;
-
-    [].concat(path).forEach(function(path) {
-        this._router.addRoute(path, noop);
-    }.bind(this));
-
-    this._doMatch();
-    ROUTES.push(this);
-}
-
-Route.prototype = {
-
-    isMatch: function() {
-        return !!this._match;
-    },
-
-    _doMatch: function() {
-        this._match = this._router.match(Nav.getCurrentPath());
-    },
-
-    get: function(key) {
-        if (!this.isMatch()) {
-            throw new Error("Cannot get key '" + key + "' for route " + this._path + " - not matched!");
-        }
-
-        return this._match.params[key];
-    }
-
-};
-
+/**
+ * Create new utils.Nav.Route instance for given path matcher
+ *
+ * @static
+ * @method createRoute
+ * @return {utils.Nav.Route}
+ */
 Nav.createRoute = function(path) {
     return new Route(path);
 };
-
-Nav.Route = Route;
 
 
 Nav.renderPathTemplate = function(tmpl, props) {
@@ -73,44 +55,6 @@ Nav.renderPathTemplate = function(tmpl, props) {
     return tmpl;
 };
 
-Nav.createLink = function(hrefTemplate, override) {
-
-    var Link = React.createClass(xtend({
-
-        go: function() {
-            Link.go(this.props);
-        },
-
-        handleClick: function(e) {
-            // Only left click navigates
-            if (e && e.button !== 0) return;
-            if (e && e.preventDefault) e.preventDefault();
-            this.go();
-        },
-
-        renderHref: function() {
-            return Link.renderHref(this.props);
-        },
-
-        render: function() {
-            return (
-                <a href={this.renderHref()} onClick={this.handleClick}>
-                    {this.props.children}
-                </a>
-            );
-        }
-    }, override));
-
-    Link.renderHref = function(props) {
-        return Nav.renderPathTemplate(hrefTemplate, props);
-    };
-
-    Link.go = function(props) {
-        Nav.go(Link.renderHref(props));
-    };
-
-    return Link;
-};
 
 
 
@@ -119,12 +63,27 @@ function updateRoutes() {
         r._doMatch();
     });
 
+    /**
+     * Called when browser navigates from pop state or Nav.go(...)
+     *
+     * User must override this.
+     *
+     * @static
+     * @method onNavigate
+     */
     if (typeof Nav.onNavigate === "function") {
         Nav.onNavigate();
     }
 }
 
 
+/**
+ * Navigate browser to a url
+ *
+ * @static
+ * @method go
+ * @param {String} url
+ */
 Nav.go = function(url) {
     history.pushState({}, "", Nav.mountPoint + url);
     console.log("navigate!", url);
@@ -133,6 +92,13 @@ Nav.go = function(url) {
 
 window.addEventListener("popstate", updateRoutes);
 
+/**
+ * Return the current browser url
+ *
+ * @static
+ * @method getCurrentPath
+ * @return {String}
+ */
 Nav.getCurrentPath = function() {
     var realPath = window.location.pathname;
     if (!Nav.mountPoint) return realPath || "/";
@@ -147,4 +113,149 @@ Nav.getCurrentPath = function() {
     return realPath || "/";
 };
 
+
+
+
+
+/**
+ * Create React Link component. When clicked browser will navigate to given
+ * href template. The template is compiled using the component props.
+ *
+ * @static
+ * @method createLink
+ * @return {utils.Nav.Link}
+ */
+Nav.createLink = function(hrefTemplate, override) {
+
+    /**
+     * @namespace utils.Nav
+     * @class Link
+     * @extends React.ReactComponent
+     */
+    var Link = React.createClass(xtend({
+
+        /**
+         * Navigate browser to the href of this component instance
+         *
+         * @method go
+         */
+        go: function() {
+            Link.go(this.props);
+        },
+
+        /**
+         * Render the href of this component instance using its props
+         *
+         * @method renderHref
+         * @return {String}
+         */
+        renderHref: function() {
+            return Link.renderHref(this.props);
+        },
+
+        handleClick: function(e) {
+            // Only left click navigates
+            if (e && e.button !== 0) return;
+            if (e && e.preventDefault) e.preventDefault();
+            this.go();
+        },
+
+        render: function() {
+            return (
+                <a href={this.renderHref()} onClick={this.handleClick}>
+                    {this.props.children}
+                </a>
+            );
+        }
+    }, override));
+
+    /**
+     * Render href for given props
+     *
+     * @static
+     * @method renderHref
+     * @param {Object} props
+     * @return {String}
+     */
+    Link.renderHref = function(props) {
+        return Nav.renderPathTemplate(hrefTemplate, props);
+    };
+
+    /**
+     * Navigate browser to hrefTemplate using the given props
+     *
+     * @static
+     * @method go
+     * @param {Object} props
+     */
+    Link.go = function(props) {
+        Nav.go(Link.renderHref(props));
+    };
+
+    return Link;
+};
+
+/**
+ * Small wrapper for routes.js
+ *
+ * https://github.com/aaronblohowiak/routes.js
+ *
+ * @namespace utils.Nav
+ * @class Route
+ * @constructor
+ * @param {String} path Path matcher string. For example `/tickets/:id`. See
+ * routes.js documentation for details
+ */
+function Route(path) {
+    this._match = null;
+    this._router = createRouter();
+    this._path = path;
+
+    [].concat(path).forEach(function(path) {
+        this._router.addRoute(path, noop);
+    }.bind(this));
+
+    this._doMatch();
+    ROUTES.push(this);
+}
+
+Route.prototype = {
+
+    /**
+     * Returns true when when the current window.location matches with this route
+     *
+     * @method isMatch
+     * @return {Boolean}
+     */
+    isMatch: function() {
+        return !!this._match;
+    },
+
+    _doMatch: function() {
+        this._match = this._router.match(Nav.getCurrentPath());
+    },
+
+    /**
+     * Get value for given route param
+     *
+     * Example:
+     *
+     * Path .get("id") for `/tickets/:id` gives `1` in url `/tickets/1`
+     *
+     * throws an error when the route does not match
+     *
+     * @method get
+     * @param {String} key
+     */
+    get: function(key) {
+        if (!this.isMatch()) {
+            throw new Error("Cannot get key '" + key + "' for route " + this._path + " - not matched!");
+        }
+
+        return this._match.params[key];
+    }
+
+};
+
+Nav.Route = Route;
 module.exports = Nav;
