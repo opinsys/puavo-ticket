e5646d8e3035a72bf7873a1c703a54739f998f46
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2014-05-14 12:20:50 +0000

    Add documentation

--- utils/Puavo.js
+++ utils/Puavo.js
@@ -4,11 +4,24 @@ var Promise = require("bluebird");
 var request = Promise.promisify(require("request"));
 var config = require("../config.js");
 
+/**
+ * Client library for puavo-rest
+ *
+ * @namespace utils
+ * @class Puavo
+ */
 function Puavo(options) {
 
     this.domain = options.domain;
 }
 
+/**
+ * Create request to the puavo-rest with authentication by organisation
+ *
+ * @method request
+ * @param {String} url
+ * @return {Object}
+ */
 Puavo.prototype.request = function(url) {
     return request(config.puavo.protocol + this.domain + url, {
         'auth': {
@@ -22,7 +35,13 @@ Puavo.prototype.request = function(url) {
 
 };
 
-
+/**
+ * Get user information by username from the puavo-rest
+ *
+ * @method userByUsername
+ * @param {String} username
+ * @return {Object}
+ */
 Puavo.prototype.userByUsername = function(username) {
     return this.request("/v3/users/" + username);
 
