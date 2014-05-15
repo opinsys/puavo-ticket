9108729f323afd407d24b978ee6a49a2b799fa3c
Esa-Matti Suuronen <esa-matti@suuronen.org>
2014-05-15 13:34:47 +0000

    Use other user as the handler

--- test/models/server/Handler_test.js
+++ test/models/server/Handler_test.js
@@ -1,8 +1,10 @@
 "use strict";
 
-var helpers = require("../../helpers");
-
 var assert = require("assert");
+var Promise = require("bluebird");
+
+var helpers = require("../../helpers");
+var User = require("../../../models/server/User");
 
 describe("Handler model", function() {
 
@@ -10,16 +12,14 @@ describe("Handler model", function() {
         var self = this;
         return helpers.clearTestDatabase()
             .then(function() {
-                return helpers.loginAsUser(helpers.user.teacher);
+                return Promise.all([
+                    User.ensureUserFromJWTToken(helpers.user.teacher),
+                    User.ensureUserFromJWTToken(helpers.user.teacher2)
+                ]);
             })
-            .then(function(agent) {
-                self.agent = agent;
-
-                return helpers.fetchTestUser();
-            })
-            .then(function(user) {
+            .spread(function(user, otherUser) {
                 self.user = user;
-
+                self.otherUser = otherUser;
                 return helpers.insertTestTickets(user);
             })
             .then(function(tickets) {
@@ -31,7 +31,11 @@ describe("Handler model", function() {
     it("can be added from a ticket", function() {
         var self = this;
 
-        return self.ticket.addHandler(self.user, self.user)
+        return User.byExternalId(helpers.user.teacher2.id)
+            .fetch({ require: true })
+            .then(function(otherUser) {
+                return self.ticket.addHandler(otherUser, self.user);
+            })
             .then(function() {
                 return self.ticket.handlers().fetch({
                         withRelated: [ "handler" ]
@@ -41,11 +45,11 @@ describe("Handler model", function() {
                 handlers = handlers.toJSON();
                 assert.equal(1, handlers.length, "has one handler");
                 assert.equal(self.user.id, handlers[0].created_by);
-                assert.equal(self.user.id, handlers[0].handler.id);
-                assert.equal("olli.opettaja", handlers[0].handler.external_data.username);
+                assert.equal(self.otherUser.id, handlers[0].handler.id);
+                assert.equal("matti.meikalainen", handlers[0].handler.external_data.username);
+
+                return self.ticket.visibilities().fetch();
             });
-
-
 
     });
 });
