983ab2cd6a68d9bb6bea2459ded4368641d82c92
Esa-Matti Suuronen <esa-matti@suuronen.org>
2014-05-28 09:34:02 +0000

    code style fix

--- models/client/Handler.js
+++ models/client/Handler.js
@@ -17,12 +17,12 @@ var Handler = Base.extend({
         var self = this;
 
         return Promise.cast($.post(this.collection.ticket.url() + "/handlers", {
-            username: this.get("user").get("external_data").username,
-            organisation_domain: this.get("user").get("external_data").organisation_domain
-        }))
-        .then(function(res) {
-            self.set(res);
-        });
+                username: this.get("user").get("external_data").username,
+                organisation_domain: this.get("user").get("external_data").organisation_domain
+            }))
+            .then(function(res) {
+                self.set(res);
+            });
     }),
 
 });
