3bdfb3475d66b1dc226979bc771caa0510aa45ec
Esa-Matti Suuronen <esa-matti@suuronen.org>
2014-06-03 11:37:19 +0000

    Document react devtools global

--- client_setup.js
+++ client_setup.js
@@ -3,5 +3,8 @@ Promise.longStackTraces();
 var $ = require("jquery");
 var Backbone = require("backbone");
 Backbone.$ = $;
+
 var React = require("react/addons");
+// React devtools requires a global access to the React object to work
+// http://fb.me/react-devtools 
 window.React = React;
