24720f3735c5c9a6677636ec7fcda0ce2f018f20
Esa-Matti Suuronen <esa-matti@suuronen.org>
2014-06-12 10:43:10 +0000

    Move manager requirement to Handler model

--- models/server/Handler.js
+++ models/server/Handler.js
@@ -17,6 +17,20 @@ var Handler = Base.extend({
 
     tableName: "handlers",
 
+    initialize: function() {
+        this.on("creating", this._assertCreatorIsManager.bind(this));
+    },
+
+    _assertCreatorIsManager: function() {
+        return User.byId(this.get("created_by"))
+            .fetch({ require: true })
+            .then(function(user) {
+                if (!user.isManager()) {
+                    throw new Error("Only managers can add handlers");
+                }
+            });
+    },
+
     defaults: function() {
         return {
             created_at: new Date(),
