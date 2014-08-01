379be8275bfdaf1d824f491e18cc4e84496289ee
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2014-08-01 15:08:37 +0300

    Fix logout handler

--- components/UserInformation.js
+++ components/UserInformation.js
@@ -19,13 +19,10 @@ var UserInformation = React.createClass({
         return this.props.user.get("externalData").first_name + " " + this.props.user.get("externalData").last_name;
     },
 
-    handleLogout: function() {
-        // TODO: redirect full page to /logout
-        alert("todo");
-    },
-
     render: function() {
         var linkToProfile = "https://" + this.props.user.get("externalData").organisation_domain + "/users/profile/edit";
+        var linkToLogout = window.location.origin + "/logout";
+
         return (
             <div className="UserInformation">
                 <img src={this.props.user.getProfileImage()} />
@@ -38,8 +35,10 @@ var UserInformation = React.createClass({
                         </a>
                     </MenuItem>
 
-                    <MenuItem onClick={this.handleLogout}>
-                        <i className="fa fa-sign-out"></i> Kirjaudu ulos
+                    <MenuItem>
+                        <a href={linkToLogout}>
+                            <i className="fa fa-sign-out"></i> Kirjaudu ulos
+                        </a>
                     </MenuItem>
 
                 </DropdownButton>
