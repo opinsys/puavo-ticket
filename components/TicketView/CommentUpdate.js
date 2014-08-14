73b756307005c9355541c67a861f3d9330449d5e
Esa-Matti Suuronen <esa-matti@suuronen.org>
2014-08-14 18:39:53 +0300

    Highlight selected comment update

--- components/TicketView/CommentUpdate.js
+++ components/TicketView/CommentUpdate.js
@@ -1,6 +1,7 @@
 /** @jsx React.DOM */
 "use strict";
 var React = require("react/addons");
+var classSet = React.addons.classSet;
 
 var ProfileBadge = require("../ProfileBadge");
 var OnViewportMixin = require("../OnViewportMixin");
@@ -18,19 +19,39 @@ var TimeAgo = require("../TimeAgo");
  */
 var CommentUpdate = React.createClass({
     mixins: [UpdateMixin, OnViewportMixin],
+
+    componentDidMount: function() {
+        window.addEventListener("hashchange", this._onHashChange);
+    },
+
+    componentWillMount: function() {
+        window.removeEventListener("hashchange", this._onHashChange);
+    },
+
+    _onHashChange: function() {
+        this.forceUpdate();
+    },
+
     render: function() {
         var update = this.props.update;
         var hashId = "comment-" + update.get("id");
+        var isSelectedByAddress = window.location.hash.slice(1) === hashId;
+
+        var classes = classSet({
+            CommentUpdate: true,
+            "ticket-updates": true,
+            selected: isSelectedByAddress
+        });
 
         return (
-            <div className="ticket-updates comments" id={hashId}>
+            <div className={classes} id={hashId}>
                 <div className="image">
                     <ProfileBadge user={update.createdBy()} />
                 </div>
                 <div className="message">
                     <strong>{update.createdBy().getFullName()} <br/></strong>
                     <ForcedLinebreaks>{update.get("comment")}</ForcedLinebreaks>
-                    <a href={"#" + hashId }>
+                    <a href={"#" + hashId } onClick={this.onHashChange}>
                         <TimeAgo date={update.createdAt()} />
                     </a>
                 </div>
