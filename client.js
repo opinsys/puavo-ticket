d0e94c1b8c4e9aa393892a52cd1f1145b9a1ac20
Esa-Matti Suuronen <esa-matti@suuronen.org>
2014-08-15 17:10:35 +0300

    Extract Main.js from client.js

--- client.js
+++ client.js
@@ -1,127 +1,29 @@
 /** @jsx React.DOM */
 "use strict";
-require("./client_setup");
+
+var Promise = require("bluebird");
+Promise.longStackTraces();
+var $ = require("jquery");
+var Backbone = require("backbone");
+Backbone.$ = $;
+
+// Load Finnish locale for React
+require("moment/locale/fi");
+
+// Make it possible to enable debug logs
+window.debug = require("debug");
 
 var React = require("react/addons");
-var Backbone = require("backbone");
+// React devtools requires a global access to the React object to work
+// http://fb.me/react-devtools
+window.React = React;
+
 var Route = require("react-router").Route;
-var Link = require("react-router").Link;
 var Routes = require("react-router").Routes;
-
-var BackboneMixin = require("./components/BackboneMixin");
-var User = require("./models/client/User");
-
+var Main = require("./components/Main");
 var TicketForm = require("./components/TicketForm");
 var TicketView = require("./components/TicketView");
 var TicketList = require("./components/TicketList");
-var ErrorMessage = require("./components/ErrorMessage");
-var UserInformation = require("./components/UserInformation");
-
-var Modal = require("react-bootstrap/Modal");
-
-
-
-/**
- * Root React component. The app starts here
- *
- * @namespace components
- * @class Main
- * @extends react.ReactComponent
- */
-var Main = React.createClass({
-
-    mixins: [BackboneMixin],
-
-    getInitialState: function() {
-        return {
-            user: new User(window.USER),
-            ticket: null
-        };
-    },
-
-    componentDidMount: function() {
-        Backbone.on("error", this.handleUnhandledError);
-    },
-
-    componentWillUnmount: function() {
-        Backbone.off("error", this.handleUnhandledError);
-    },
-
-    handleUnhandledError: function(error, customMessage) {
-        console.error(customMessage + ":", error.message);
-        if (error.stack) console.error(error.stack);
-        this.renderInModal("Uups! Jotain odottamatonta tapahtui :(", function(){
-            return <ErrorMessage error={error} customMessage={customMessage} />;
-        }, function() {
-            window.scrollTo(0, 0);
-        });
-    },
-
-
-    /**
-     * @method renderInModal
-     * @param {String} title
-     * @param {Function} renderModalContent
-     *      Function returning a React component
-     * @param {Function} renderModalContent.close
-     *      Call this function to close the modal window
-     */
-    renderInModal: function(title, render, cb) {
-        var self = this;
-        this.setState({ renderModalContent: function() {
-            return (
-                <Modal
-                    onRequestHide={function(){}}
-                    title={title} >
-                    {render(self.closeModal)}
-                </Modal>
-            );
-        } }, cb);
-    },
-
-    closeModal: function(e) {
-        this.setState({ renderModalContent: null });
-    },
-
-    render: function() {
-        return (
-            <div className="wrapper container-fluid">
-                <h1 className="site-header">Opinsys tukipalvelu</h1>
-                {this.state.renderModalContent && this.state.renderModalContent()}
-                <div className="topmenu row">
-
-                    <div className="user-info pull-right">
-                        <UserInformation user={this.state.user} />
-                    </div>
-
-                    <div className="top-buttons">
-
-                        <Link className="btn btn-default top-button" to="new">
-                            <i className="fa fa-pencil-square-o"></i>Uusi tukipyyntö
-                        </Link>
-
-                        <Link className="btn btn-default top-button" to="tickets">
-                            <i className="fa fa-home"></i>Omat tukipyynnöt
-                        </Link>
-
-                    </div>
-                </div>
-
-                <div className="main-wrap clearfix" >
-                    <div className="main">
-
-                        <this.props.activeRouteHandler
-                            renderInModal={this.renderInModal}
-                            user={this.state.user} />
-
-                    </div>
-
-                </div>
-            </div>
-        );
-    }
-
-});
 
 React.renderComponent(
     <Routes location="history">
---
+++ components/Main.js
@@ -0,0 +1,117 @@
+/** @jsx React.DOM */
+"use strict";
+
+var React = require("react/addons");
+var Backbone = require("backbone");
+var Link = require("react-router").Link;
+var Modal = require("react-bootstrap/Modal");
+
+var User = require("../models/client/User");
+var BackboneMixin = require("./BackboneMixin");
+var ErrorMessage = require("./ErrorMessage");
+var UserInformation = require("./UserInformation");
+
+
+/**
+ * Root React component. The app starts here
+ *
+ * @namespace components
+ * @class Main
+ * @extends react.ReactComponent
+ */
+var Main = React.createClass({
+
+    mixins: [BackboneMixin],
+
+    getInitialState: function() {
+        return {
+            user: new User(window.USER),
+            ticket: null
+        };
+    },
+
+    componentDidMount: function() {
+        Backbone.on("error", this.handleUnhandledError);
+    },
+
+    componentWillUnmount: function() {
+        Backbone.off("error", this.handleUnhandledError);
+    },
+
+    handleUnhandledError: function(error, customMessage) {
+        console.error(customMessage + ":", error.message);
+        if (error.stack) console.error(error.stack);
+        this.renderInModal("Uups! Jotain odottamatonta tapahtui :(", function(){
+            return <ErrorMessage error={error} customMessage={customMessage} />;
+        }, function() {
+            window.scrollTo(0, 0);
+        });
+    },
+
+
+    /**
+     * @method renderInModal
+     * @param {String} title
+     * @param {Function} renderModalContent
+     *      Function returning a React component
+     * @param {Function} renderModalContent.close
+     *      Call this function to close the modal window
+     */
+    renderInModal: function(title, render, cb) {
+        var self = this;
+        this.setState({ renderModalContent: function() {
+            return (
+                <Modal
+                    onRequestHide={function(){}}
+                    title={title} >
+                    {render(self.closeModal)}
+                </Modal>
+            );
+        } }, cb);
+    },
+
+    closeModal: function(e) {
+        this.setState({ renderModalContent: null });
+    },
+
+    render: function() {
+        return (
+            <div className="wrapper container-fluid">
+                <h1 className="site-header">Opinsys tukipalvelu</h1>
+                {this.state.renderModalContent && this.state.renderModalContent()}
+                <div className="topmenu row">
+
+                    <div className="user-info pull-right">
+                        <UserInformation user={this.state.user} />
+                    </div>
+
+                    <div className="top-buttons">
+
+                        <Link className="btn btn-default top-button" to="new">
+                            <i className="fa fa-pencil-square-o"></i>Uusi tukipyyntö
+                        </Link>
+
+                        <Link className="btn btn-default top-button" to="tickets">
+                            <i className="fa fa-home"></i>Omat tukipyynnöt
+                        </Link>
+
+                    </div>
+                </div>
+
+                <div className="main-wrap clearfix" >
+                    <div className="main">
+
+                        <this.props.activeRouteHandler
+                            renderInModal={this.renderInModal}
+                            user={this.state.user} />
+
+                    </div>
+
+                </div>
+            </div>
+        );
+    }
+
+});
+
+module.exports = Main;
