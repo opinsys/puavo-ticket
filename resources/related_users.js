9c9ce123622679a4f539534fdee1c56c8fc86abd
Esa-Matti Suuronen <esa-matti@suuronen.org>
2014-08-19 13:16:41 +0300

    Remove GET /api/tickets/:id/relatedUsers
    Not using it

--- resources/related_users.js
+++ resources/related_users.js
@@ -7,7 +7,6 @@ var express = require("express");
 var Promise = require("bluebird");
 
 var Ticket = require("../models/server/Ticket");
-var RelatedUser = require("../models/server/RelatedUser");
 var User = require("../models/server/User");
 
 var app = express.Router();
@@ -36,21 +35,5 @@ app.post("/api/tickets/:id/relatedUsers", function(req, res, next) {
     .catch(next);
 });
 
-/**
- * @api {get} /api/tickets/:id/relatedUsers Get related users for a ticket
- * @apiName GetRelatedUsers
- * @apiGroup relatedUsers
- *
- * @apiSuccess {Object[]} . List of related users
- */
-app.get("/api/tickets/:id/relatedUsers", function(req, res, next) {
-    RelatedUser.collection()
-    .query('where', 'ticketId', '=', req.params.id)
-    .fetch()
-    .then(function(collection) {
-        res.json(collection.toJSON());
-    })
-    .catch(next);
-});
 
 module.exports = app;
