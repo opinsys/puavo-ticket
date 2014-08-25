72e730dff09a39343331e1342aa0b16830d86cd5
Esa-Matti Suuronen <esa-matti@suuronen.org>
2014-08-25 17:07:49 +0300

    Add borders to SideInfo

--- components/SideInfo.js
+++ components/SideInfo.js
@@ -15,11 +15,11 @@ var SideInfo = React.createClass({
     render: function() {
         return (
             <div className="SideInfo">
-                <div className="contact">
+                <div className="contact box">
                     <p>Kiireellisissä tapauksissa <br/> soita tukinumeroomme <strong>014-4591625</strong></p>
                 </div>
 
-                <div className="checklist">
+                <div className="checklist box">
                     <p><strong>Ilmoita nämä asiat tukipyynnössä</strong></p>
                     <ul>
                         <li>tarkka kuvaus tuen tarpeesta</li>
