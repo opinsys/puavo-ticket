12f3b0084dc0078518f66bdfa3ed336a55d9adf9
Esa-Matti Suuronen <esa-matti@suuronen.org>
2014-08-27 14:36:12 +0300

    Drop obscure helpers.insertTestTickets(...) usage

--- test/api/notifications_test.js
+++ test/api/notifications_test.js
@@ -1,37 +1,50 @@
 "use strict";
 var _ = require("lodash");
 var assert = require("assert");
+var Promise = require("bluebird");
 
 var helpers = require("../helpers");
 var Notification = require("../../models/server/Notification");
+var User = require("../../models/server/User");
+var Ticket = require("../../models/server/Ticket");
 
 
 
 describe("/api/tickets/:id/read", function() {
 
-    var ticket = null;
-    var otherTicket = null;
-
     before(function() {
         var self = this;
-
         return helpers.clearTestDatabase()
             .then(function() {
-                return helpers.loginAsUser(helpers.user.teacher);
+                return Promise.join(
+                    User.ensureUserFromJWTToken(helpers.user.manager),
+                    User.ensureUserFromJWTToken(helpers.user.teacher),
+                    User.ensureUserFromJWTToken(helpers.user.teacher2)
+                );
             })
-            .then(function(agent) {
-                self.agent = agent;
-
-                return helpers.fetchTestUser();
-            })
-            .then(function(user) {
+            .spread(function(manager, user, otherUser) {
+                self.manager = manager;
                 self.user = user;
+                self.otherUser = otherUser;
 
-                return helpers.insertTestTickets(user);
+                return Promise.join(
+                    Ticket.create(
+                        "The Ticket",
+                        "Will get notifications",
+                        self.user
+                    ),
+                    Ticket.create(
+                        "An other ticket",
+                        "This is other ticket without any notifications for the user",
+                        self.otherUser
+                    ),
+                    helpers.loginAsUser(helpers.user.teacher)
+                );
             })
-            .then(function(tickets) {
-                ticket = tickets.ticket;
-                otherTicket = tickets.otherTicket;
+            .spread(function(ticket, otherTicket, agent) {
+                self.ticket = ticket;
+                self.otherTicket = otherTicket;
+                self.agent = agent;
             });
     });
 
@@ -41,8 +54,9 @@ describe("/api/tickets/:id/read", function() {
 
 
     it("can mark ticket as read", function() {
+        var self = this;
         return this.agent
-            .post("/api/tickets/" + ticket.get("id") + "/read")
+            .post("/api/tickets/" + self.ticket.get("id") + "/read")
             .send()
             .promise()
             .then(function(res) {
@@ -51,24 +65,23 @@ describe("/api/tickets/:id/read", function() {
                 assert.equal("notifications", res.body.type);
             })
             .then(function() {
-                return Notification.forge({ ticketId: ticket.get("id") }).fetch();
+                return Notification.forge({ ticketId: self.ticket.get("id") }).fetch();
             })
             .then(function(notification) {
-                assert.equal(notification.get("ticketId"), ticket.get("id"));
+                assert.equal(notification.get("ticketId"), self.ticket.get("id"));
             });
     });
 
     it("can see is ticket as read", function() {
         var self = this;
-
         return this.agent
             .get("/api/tickets")
             .promise()
             .then(function(res) {
                 assert.equal(res.status, 200);
-                assert(_.find(res.body[0].titles, { title: "Test ticket title" }));
-                assert.equal(ticket.get("id"), res.body[0].notifications[0].ticketId);
-                assert.equal(self.user.id, res.body[0].notifications[0].targetId);
+                assert(_.find(res.body[0].titles, { title: "The Ticket" }));
+                assert.equal(self.ticket.get("id"), res.body[0].notifications[0].ticketId);
+                assert.equal(self.user.get("id"), res.body[0].notifications[0].targetId);
             });
     });
 
