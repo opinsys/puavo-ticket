c3d158aaa065d8b9ad6b2125d3d1c61eaa51284c
Esa-Matti Suuronen <esa-matti@suuronen.org>
2014-09-26 20:07:05 +0300

    Add delete all tickets helper

---
+++ extra/deleteAllTickets.js
@@ -0,0 +1,63 @@
+"use strict";
+var DB = require("app/db");
+/**
+ * Delete all rows from given tables in series.
+ *
+ * The rows must have an id sequence which will be restarted
+ *
+ * @static
+ * @private
+ * @method deleteAndReset
+ * @param {Array} tables Tables names
+ */
+function deleteAndReset(tables) {
+    if (tables.length === 0) return;
+
+    var tableName = tables[0];
+
+    return DB.knex(tableName).del()
+    .then(function() {
+        return DB.knex.raw("ALTER SEQUENCE \"" + tableName + "_id_seq\" RESTART");
+    })
+    .then(function() {
+        return deleteAndReset(tables.slice(1));
+    });
+}
+
+/**
+ * Ensure empty database for testing
+ *
+ * @static
+ * @method clearTestDatabase
+ * @return {Bluebird.Promise}
+ */
+function deleteAllTickets() {
+    // the chunks table has no incrementing id column
+    return DB.knex("chunks").del()
+    .then(function() {
+        return deleteAndReset([
+            "attachments",
+            "comments",
+            "visibilities",
+            "relatedUsers",
+            "devices",
+            "followers",
+            "tags",
+            "handlers",
+            "notifications",
+            "titles",
+            "tickets",
+        ]);
+    });
+}
+
+deleteAllTickets()
+.then(function() {
+    console.log("done");
+    process.exit();
+})
+.catch(function(err) {
+    console.log(err);
+    console.log(err.stack);
+    process.exit(1);
+});
