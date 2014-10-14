1ebe708b466e4e0761e1a1fe57165f80c8ba57df
Esa-Matti Suuronen <esa-matti@suuronen.org>
2014-10-14 07:53:04 +0000

    Add font-awesome wrapper component

---
+++ components/Fa.js
@@ -0,0 +1,51 @@
+/** @jsx React.DOM */
+"use strict";
+
+var React = require("react/addons");
+var classSet = React.addons.classSet;
+
+/**
+ * Font-Awesome wrapper
+ *
+ * http://fontawesome.io/icons/
+ *
+ * @namespace components
+ * @class Fa
+ * @constructor
+ * @param {Object} props
+ * @param {Object} props.icon Icon key without the "fa-"Jj prefix
+ */
+var Fa = React.createClass({
+
+    propTypes: {
+        icon: React.PropTypes.string.isRequired,
+        spin: React.PropTypes.bool,
+        visible: React.PropTypes.bool,
+    },
+
+    getDefaultProps: function() {
+        return {
+            visible: true,
+        };
+    },
+
+    getStyles: function() {
+        var val = this.props.visible ? "visible" : "hidden";
+        return { visibility: val };
+    },
+
+    render: function() {
+        var className = classSet({
+            fa: true,
+            "fa-spin": this.props.spin,
+        });
+
+        className += " fa-" + this.props.icon;
+        className += " " + this.props.className;
+        return (
+            <i className={className.trim()} style={this.getStyles()}></i>
+        );
+    }
+});
+
+module.exports = Fa;
