85de86c9115fbf6c8370189f10334297642c3ce9
Esa-Matti Suuronen <esa-matti@suuronen.org>
2014-10-16 14:59:24 +0000

    Halt promise chains when capturing fatal errors

--- utils/captureError.js
+++ utils/captureError.js
@@ -1,10 +1,14 @@
 "use strict";
 
+var Promise = require("bluebird");
 var Backbone = require("backbone");
 
 function captureError(customMessage) {
     return function(err) {
         Backbone.trigger("error", err, customMessage);
+        return new Promise(function(resolve, reject){
+            // Halt the promise chain by never resolving this
+        });
     };
 }
 
