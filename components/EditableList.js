967e5c20979a68cba8ffcf1985ac5b581038db84
Esa-Matti Suuronen <esa-matti@suuronen.org>
2014-10-16 15:00:10 +0000

    Add disabled prop for EditableList

--- components/EditableList.js
+++ components/EditableList.js
@@ -36,6 +36,7 @@ var Item = React.createClass({
         className: React.PropTypes.string,
         onRemove: React.PropTypes.func,
         permanent: React.PropTypes.bool,
+        disabled: React.PropTypes.bool,
     },
 
     getInitialState: function() {
@@ -48,13 +49,14 @@ var Item = React.createClass({
     render: function() {
         var self = this;
         var className = "Item " + this.props.className;
+        var disabled = !!this.props.disabled;
         var pending = this.state.removePending;
         return (
             <li className={className} key={this.props.key}>
                 {!this.props.permanent && <Button className="remove-button"
                         bsStyle="danger"
                         bsSize="xsmall"
-                        disabled={pending}
+                        disabled={pending || disabled}
                         onClick={function() {
                             self.setState({ removePending: true });
                             self.props.onRemove(self.props);
