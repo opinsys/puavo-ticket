98f05c1bc5095c3346062305a56d873b2ac4d195
Esa-Matti Suuronen <esa-matti@suuronen.org>
2014-10-20 14:44:25 +0000

    Capture only one error

--- utils/captureError.js
+++ utils/captureError.js
@@ -3,8 +3,11 @@
 var Promise = require("bluebird");
 var Backbone = require("backbone");
 
+var captured = false;
 function captureError(customMessage) {
     return function(err) {
+        if (captured) return;
+        captured = true;
         Backbone.trigger("error", err, customMessage);
         return new Promise(function(resolve, reject){
             // Halt the promise chain by never resolving this
