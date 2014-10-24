dd36fa5b923f8df1fff05993f31479affc137214
Esa-Matti Suuronen <esa-matti@suuronen.org>
2014-10-24 06:58:35 +0000

    Restore DISPLAY=:99
    Used by the client unit tests

--- ci.sh
+++ ci.sh
@@ -13,6 +13,8 @@ sudo apt-get update
 sudo make install-ansible
 sudo ansible-playbook development-env.yml --extra-vars dev_user=$USER --extra-vars code_dest=/cirun --extra-vars archive_server=new-archive.opinsys.fi
 
+export DISPLAY=:99
+
 # XXX: For some random reason ci user has no permissions to write
 # /home/ci/.npmrc
 # Workaround by using sudo.
