3bc8d926c4f85e1b92ab2cf088bdfffa5a82f59d
Esa-Matti Suuronen <esa-matti@suuronen.org>
2014-10-30 13:56:58 +0000

    Build fixup

--- ci.sh
+++ ci.sh
@@ -14,16 +14,17 @@ sudo apt-get install -y --force-yes aptirepo-upload puavo-devscripts wget
 
 export DISPLAY=:99
 
-# XXX: For some random reason ci user has no permissions to write
-# /home/ci/.npmrc
-# Workaround by using sudo.
-sudo npm set registry http://registry.npmjs.org/
 
 export NODE_ENV=test
 
 wget -qO - https://github.com/opinsys/puavo-standalone/raw/master/setup.sh | sudo sh
 
 sudo make install-build-dep
+# XXX: For some random reason ci user has no permissions to write
+# /home/ci/.npmrc
+# Workaround by using sudo.
+sudo npm set registry http://registry.npmjs.org/
+
 make deb
 
 aptirepo-upload -r $APTIREPO_REMOTE -b "git-$(echo "$GIT_BRANCH" | cut -d / -f 2)" ../puavo-ticket*.changes
