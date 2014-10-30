5a09a901a7b20efb1ae5981113d480896f17a36b
Esa-Matti Suuronen <esa-matti@suuronen.org>
2014-10-30 14:29:15 +0000

    Clean up ci.sh

--- ci.sh
+++ ci.sh
@@ -2,29 +2,25 @@
 
 set -eux
 
-set
-
-env
-
-## fix home for now
-export HOME=/home/ci
-
-sudo apt-get update
-sudo apt-get install -y --force-yes aptirepo-upload puavo-devscripts wget
-
+export HOME=/home/ci ## fix home for now
 export DISPLAY=:99
-
-
 export NODE_ENV=test
 
+sudo apt-get install wget
+
+# Apply puavo-standalone Ansible rules
 wget -qO - https://github.com/opinsys/puavo-standalone/raw/master/setup.sh | sudo sh
 
+# Install build dependencies
 sudo make install-build-dep
+
 # XXX: For some random reason ci user has no permissions to write
 # /home/ci/.npmrc
 # Workaround by using sudo.
 sudo npm set registry http://registry.npmjs.org/
 
+# Build debian package
 make deb
 
+# Upload it to archive.opinsys.fi
 aptirepo-upload -r $APTIREPO_REMOTE -b "git-$(echo "$GIT_BRANCH" | cut -d / -f 2)" ../puavo-ticket*.changes
