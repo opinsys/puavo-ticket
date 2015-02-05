eeb2defd190b8895ab44b4b98b660a659a3c235e
Esa-Matti Suuronen <esa-matti@suuronen.org>
2015-02-05 14:52:09 +0200

    Refactor Handler test for es6

--- test/models/server/Handler_test.js
+++ test/models/server/Handler_test.js
@@ -10,84 +10,71 @@ var Ticket = require("app/models/server/Ticket");
 
 describe("Ticket handlers", function() {
 
-    before(function() {
-        var self = this;
-        return helpers.clearTestDatabase()
-            .then(function() {
-                return Promise.all([
-                    User.ensureUserFromJWTToken(helpers.user.manager),
-                    User.ensureUserFromJWTToken(helpers.user.teacher),
-                    User.ensureUserFromJWTToken(helpers.user.teacher2)
-                ]);
-            })
-            .spread(function(manager, user, otherUser) {
-                self.manager = manager;
-                self.user = user;
-                self.otherUser = otherUser;
+    var manager, teacher, teacher2, ticket;
 
-                return Ticket.create(
-                    "A title handler",
-                    "Handler test ticket",
-                    self.user
-                );
-            })
-            .then(function(ticket) {
-                self.ticket = ticket;
-            });
-    });
+    before(() =>
+       helpers.clearTestDatabase()
+       .then(() => Promise.join(
+           User.ensureUserFromJWTToken(helpers.user.manager)
+           .then((u) => manager = u),
 
-    it("can be added from a ticket", function() {
-        var self = this;
+            User.ensureUserFromJWTToken(helpers.user.teacher)
+            .then((u) => teacher = u),
 
-            return self.ticket.addHandler(self.otherUser, self.manager)
-            .then(function() {
-                return self.ticket.handlers().fetch({
-                        withRelated: [ "handler" ]
-                    });
-            })
-            .then(function(handlers) {
-                handlers = handlers.toJSON();
+            User.ensureUserFromJWTToken(helpers.user.teacher2)
+            .then((u) => teacher2 = u)
+       ))
+       .then(() => Ticket.create(
+            "A title handler",
+            "Handler test ticket",
+            teacher
+       ))
+       .then((t) => ticket = t)
 
-                var handler = _.find(handlers, function(h) {
-                    return h.handler.id === self.otherUser.id;
-                });
-
-                assert(handler, "has the other user as handler");
-
-                assert.equal(
-                    handler.createdById,
-                    self.manager.get("id"),
-                    "manager is the creator"
-                );
+    );
 
 
-            });
-    });
+    it("can be added from a ticket", () =>
+        ticket.addHandler(teacher2, manager)
+        .then(() => ticket.handlers().fetch({
+            withRelated: "handler"
+        }))
+        .then((handlers) => {
+            handlers = handlers.toJSON();
 
-    it("returns true (promise) from Ticket#isHandler(user) for handlers", function() {
-        var self = this;
-        return Promise.all([
+            var handler = _.find(handlers, (h) =>
+                h.handler.id === teacher2.id
+            );
+
+            assert(handler, "has the other user as handler");
+
+            assert.equal(
+                handler.createdById,
+                manager.get("id"),
+                "manager is the creator"
+            );
+
+        })
+    );
+
+    it("returns true (promise) from Ticket#isHandler(user) for handlers", () =>
+        Promise.join(
             User.byExternalId(helpers.user.teacher.id).fetch({ require: true }),
-            self.ticket.load("handlerUsers")
-        ])
-        .spread(function(user) {
-            assert(self.ticket.isHandler(user) === true);
-        });
-    });
+            ticket.load("handlerUsers")
+        )
+        .spread((user) => {
+            assert(ticket.isHandler(user) === true);
+        })
+    );
 
 
-    it("personal visibility is given to the handler", function() {
-        var self = this;
-
-        return self.ticket.visibilities().fetch()
-            .then(function(visibilities) {
-                visibilities = visibilities.map(function(v) {
-                    return v.get("entity");
-                });
-
-                assert(visibilities.indexOf(self.user.getPersonalVisibility()) !== -1);
-            });
-    });
+    it("personal visibility is given to the handler", () =>
+        ticket.visibilities().fetch()
+        .then((visibilities) => {
+            visibilities = visibilities.map((v) => v.get("entity"));
+            assert(visibilities.indexOf(teacher.getPersonalVisibility()) !== -1);
+        })
+    );
 
 
 });
