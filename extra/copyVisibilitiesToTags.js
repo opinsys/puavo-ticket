10d00180e0d746a2aeaaa60468c37a0070f5b972
Esa-Matti Suuronen <esa-matti@suuronen.org>
2015-02-17 13:59:17 +0200

    Add some concurrency to copyVisibilitiesToTags

--- extra/copyVisibilitiesToTags.js
+++ extra/copyVisibilitiesToTags.js
@@ -6,17 +6,23 @@ require("../db");
 var User = require("../models/server/User");
 var Visibility = require("../models/server/Visibility");
 
+var i = 0;
+var size = 0;
+
 function migrateData() {
     return User.collection().fetch()
     .then(function(c) {
         console.log("Adding accessTags for", c.size(), "users");
+        size = c.size();
         return c.models;
     })
-    .each(function(user) {
+    .map(function(user) {
         var tag = "user:" + user.get("id");
+        if (++i % 100 === 0) console.log(i, "/", size,  "accessTag", tag);
         return user.addAccessTag(tag, user);
-    })
+    }, {concurrency:5})
     .then(function() {
+        console.log("Done. Fetching visibilities...");
         return Visibility.collection()
         .query(function(q) {
             q.where("entity", "like", "user:%");
@@ -27,12 +33,15 @@ function migrateData() {
     })
     .then(function(c) {
         console.log("Adding tags for", c.size(), "tickets");
+        i = 0;
+        size = c.size();
         return c.models;
     })
-    .each(function(vis) {
+    .map(function(vis) {
         var tag = vis.get("entity");
+        if (++i % 100 === 0) console.log(i, "/", size,  "tag", tag);
         return vis.rel("ticket").addTag(tag, vis.get("createdById"));
-    });
+    }, {concurrency:5});
 }
 
 if (require.main === module) {
